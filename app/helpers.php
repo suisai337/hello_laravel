4810eefbecdc7c4c3d294c397f6cf15e2fbd6827
Yanfan <330483691@qq.com>
2019-03-15 10:33:34 +0000

    fix

--- app/helpers.php
+++ app/helpers.php
@@ -8,17 +8,18 @@
 
 function get_db_config()
 {
-    if (getenv('ID_IN_HEROKU')) {
-        $url = parse_url(getenv('DATABASE_URL'));
-        return [
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
             'connection' => 'pgsql',
-            'host' => $url['host'],
-            'database' => substr($url["path"], 1),
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
         ];
     } else {
-        return [
+        return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
             'host' => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
